038375f134e5296647b1adaea59b313ceed83d95
Diego Perez <diegoperez1295@gmail.com>
2020-06-27 12:19:41 -0400

    add mqtt

--- serial-monitor/serial-raspi.py
+++ serial-monitor/serial-raspi.py
@@ -1,9 +1,18 @@
 
 from serial import Serial
 import time
+import paho.mqtt.client as mqtt
 
 port = 'COM3'
 baud_rate = 115200
+
+try:
+    client = mqtt.Client()
+    client.connect("loacalhost", 1883, 60)
+    client.loop_start()
+    mqtt = True
+except:
+    mqtt = False
 
 if __name__ == '__main__':
     #inicia Serial
@@ -13,28 +22,34 @@ if __name__ == '__main__':
     # Obtiene fecha inicial
     date = time.strftime("%Y%m%d")
 
-    # Abre archivo Log
-    log = open(date + '.log', 'a')
-
     # Ciclo infinito
     while True:
         # Espera hasta que el serial reciba informacion
         if ser.in_waiting > 0:
+            # Abre archivo Log
+            log = open('logs/' + date + '.log', 'a')
+
             # Agrega hora
             line = time.strftime("%H:%M:%S") + ' | '
+            
             # Lee la linea del Serial
             line += ser.readline().decode().rstrip()
             line += '\n'
+            
             # Escribe en el log
             log.write(line)
+            
             #imprime
             print(line)
 
+            if mqtt:
+                #publica en mqtt
+                mqttc.publish("/serial", line)
+
+            log.close()
             # Si la fecha inicial no coincide con la nueva fecha tomada
             # abre un nuevo archivo con la nueva fecha
             if date != time.strftime("%Y%m%d"):
                 date = time.strftime("%Y%m%d")
-                log.close()
-                log = open(date + '.log', 'a')
     
     log.close()
